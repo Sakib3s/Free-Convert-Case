88195055870be610d1c9396f4f6af43e83cc1ff5
sakib3s <3s.sakibhasan@gmail.com>
2022-11-03 18:17:48 +0600

    Finished Convert Case 100%

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 (function($){
 	"use strict";
 
+    function capitalizedCase(string) {
+        return string.toString().replace(/(?:^|\s)\w/g, function(match) {
+            return match.toUpperCase();
+        });
+    }
+
+    function firstLetterUpper(string) {
+        var newString = string.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g,function(c){return c.toUpperCase()});
+        return newString;
+    }
+
+    function alternateCase(string) {
+        var chars = string.toLowerCase().split("");
+        for (var i = 0; i < chars.length; i += 2) {
+            chars[i] = chars[i].toUpperCase();
+        }
+        return chars.join("");
+    }
+
+    function inverseCase(string) {
+        var chars = string.toUpperCase().split("");
+        for (var i = 0; i < chars.length; i += 2) {
+            chars[i] = chars[i].toLowerCase();
+        }
+        return chars.join("");
+    };
+
+    function toTitleCase(string) {
+        return string.replace(/(?:^|\s)\w/g, function(match) {
+            return match.toUpperCase();
+        });
+    }
+
     $('#UpperCase').click(function(){
         $('#ConvertCaseField').val($('#ConvertCaseField').val().toUpperCase()); 
     });
@@ -15,6 +48,42 @@
 
     $('#ConvertCaseField').click(function(){
         $('.btn-copy').removeClass('copy-active'); 
+    });
+
+    $('#CapitalizedCase').click(function(){
+        var content = capitalizedCase($('#ConvertCaseField').val());
+        $('#ConvertCaseField').val(content);
+    });
+
+    $('#SentenceCase').click(function(){
+        var content = firstLetterUpper($('#ConvertCaseField').val());
+        $('#ConvertCaseField').val(content);
+    });
+
+    $('#AlternatingCase').click(function(){
+        var content = alternateCase($('#ConvertCaseField').val());
+        $('#ConvertCaseField').val(content);
+    });
+
+    $('#TitleCase').click(function(){
+        var content = toTitleCase($('#ConvertCaseField').val());
+        $('#ConvertCaseField').val(content);
+    });
+
+    $('#InverseCase').click(function(){
+        var content = inverseCase($('#ConvertCaseField').val());
+        $('#ConvertCaseField').val(content);
+    });
+
+    $('#clear').click(function(){
+        $('#ConvertCaseField').val('');
+    });
+
+    $('#ConvertCaseField').click(function(){
+        var content = $('#ConvertCaseField').val();
+        if(content == 'Type or paste your content here') {
+            $('#ConvertCaseField').val('');
+        }
     });
 
 }(jQuery));
